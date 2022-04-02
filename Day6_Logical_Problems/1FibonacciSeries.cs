2e621852c7c514950af9e542e2d602101d3f64d2
divatepratik10 <pratikrdivate3210@gmail.com>
2022-04-02 18:47:05 +0530

    [Pratik] added:Fibonacci Series Programm

---
+++ Day6_Logical_Problems/1FibonacciSeries.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day6_Logical_Problems
+{
+    internal class _1FibonacciSeries
+    {
+        public static void FiboSeries()
+        {
+            Console.WriteLine("Welcome to Fibonacci Series Programme.");
+            Console.WriteLine("Enter number of elements you want : ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int n1 = 0, n2 = 1, n3;
+            Console.WriteLine("    {0}\n    {1}", n1, n2);
+            for(int i = 2;i<num;i++)
+            {
+                n3 = n1 + n2;
+                Console.WriteLine("    {0}", n3);
+                n1 = n2;
+                n2 = n3; ;
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Day6_Logical_Problems/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day6_Logical_Problems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to DAY-06 Logical Problem Programms.");
+            _1FibonacciSeries.FiboSeries();
+                Console.ReadLine();
+        }
+    }
+}
