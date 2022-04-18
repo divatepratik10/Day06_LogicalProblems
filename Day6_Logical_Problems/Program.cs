eb09d371d784766a13f6d94584ddd6cdcdf0de99
divatepratik10 <pratikrdivate3210@gmail.com>
2022-04-18 22:43:09 +0530

    [Pratik] added:Temperature Conversion programme

--- Day6_Logical_Problems/Program.cs
+++ Day6_Logical_Problems/Program.cs
@@ -13,7 +13,7 @@ namespace Day6_Logical_Problems
             Console.WriteLine("Welcome to DAY-06 Logical Problem Programms.\n");
             Console.WriteLine("Choose between following options.\n");
             Console.WriteLine("1 : Fibonacci_Series\n2 : Perfect_Number\n3 : Prime_Number\n4 : Reverse_Number\n5 : Coupen_Number\n" +
-                "6 : Stopwatch\n7 : Vending_Machine\n8 : Day_of_Week");
+                "6 : Stopwatch\n7 : Vending_Machine\n8 : Day_of_Week\n8 : Temperature_Convert\n9 : \n10 : ");
             int n = Convert.ToInt32(Console.ReadLine());
             switch(n)
             {
@@ -41,6 +41,15 @@ namespace Day6_Logical_Problems
                     break;
                 case 8:
                     DayofWeek.FindDay();
+                    break;
+                case 9:
+                    TempConversion.TemperatureConvert();
+                    break;
+                case 10:
+                    break;
+                case 11:
+                    break;
+                case 12:
                     break;
                 default:
                     Console.WriteLine("something went wrong........");
---
+++ Day6_Logical_Problems/TempConversion.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day6_Logical_Problems
+{
+    internal class TempConversion
+    {
+        public static void TemperatureConvert()
+        {
+            Console.WriteLine("Welcome to Temperature Conversion Programme.");
+            Console.WriteLine("Enter Temperature : ");
+            double temperature = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter Temperature Unit. Type( 1-Celcius / 2-Fahrenheit )");
+            int unit = Convert.ToInt32(Console.ReadLine());
+
+            switch(unit)
+            {
+                case 1:
+                    double res = (temperature * 9 / 5) + 32;
+                    Console.WriteLine("{0} C temperature converted into {1} F. ",temperature,res);
+                    break;
+                case 2:
+                    double res1 = (temperature - 32) * 5 / 9;
+                    Console.WriteLine("{0} F temperature converted into {1} C. ", temperature, res1);
+                    break;
+                default:
+                    Console.WriteLine("Enter Valid Inputs..");
+                    break;
+            }
+        }
+    }
+}
