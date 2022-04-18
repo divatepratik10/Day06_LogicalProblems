e31c50d09aefc8b907d27fdca650f12f64303072
divatepratik10 <pratikrdivate3210@gmail.com>
2022-04-18 20:11:22 +0530

    [Pratik] added:Day of Week programme

--- Day6_Logical_Problems/Program.cs
+++ Day6_Logical_Problems/Program.cs
@@ -13,7 +13,7 @@ namespace Day6_Logical_Problems
             Console.WriteLine("Welcome to DAY-06 Logical Problem Programms.\n");
             Console.WriteLine("Choose between following options.\n");
             Console.WriteLine("1 : Fibonacci_Series\n2 : Perfect_Number\n3 : Prime_Number\n4 : Reverse_Number\n5 : Coupen_Number\n" +
-                "6 : Stopwatch\n7 : Vending_Machine");
+                "6 : Stopwatch\n7 : Vending_Machine\n8 : Day_of_Week");
             int n = Convert.ToInt32(Console.ReadLine());
             switch(n)
             {
@@ -38,6 +38,9 @@ namespace Day6_Logical_Problems
                     break;
                 case 7:
                     VendingMachine.ReturnNotes();
+                    break;
+                case 8:
+                    DayofWeek.FindDay();
                     break;
                 default:
                     Console.WriteLine("something went wrong........");
